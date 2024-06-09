85aadef1aeed1d348c56cc9bdbd6c59555d37f29
andresbluna <andres.benjamin.luna@gmail.com>
2024-06-08 22:18:25 -0400

    Updated exceptions in data read

--- loader_data.py
+++ loader_data.py
@@ -1,12 +1,15 @@
 def leer_archivo(nombre_archivo):
-    archivo = open(nombre_archivo, encoding='utf-8')
-    datos = []
-    for linea in archivo:
-        linea = linea.rstrip('\n')
-        lista = linea.split(',')
-        datos.append(lista)
-    archivo.close()
-    return datos
+    try:
+        with open(nombre_archivo, encoding='utf-8') as archivo:
+            datos = []
+            for linea in archivo:
+                linea = linea.rstrip('\n')
+                lista = linea.split(',')
+                datos.append(lista)
+        return datos
+    except Exception as e:
+        print(f"Error al leer el archivo: {e}")
+        return []
 
 def separar(datos, indices):
     final = []
@@ -18,14 +21,17 @@ def separar(datos, indices):
     return final
 
 def escribir_archivo(nombre_archivo, datos):
-    archivo = open(nombre_archivo, 'w', encoding='utf-8')
-    for linea in datos:
-        archivo.write(','.join(linea) + '\n')
-    archivo.close()
+    try:
+        with open(nombre_archivo, 'w', encoding='utf-8') as archivo:
+            for linea in datos:
+                archivo.write(','.join(linea) + '\n')
+    except Exception as e:
+        print(f"Error al escribir el archivo: {e}")
 
-    #main function:
-
-    datos_archivo = leer_archivo('Data.csv')
-    indices_a_seleccionar = [3, 4, 5, 6, 7]
-    datos_seleccionados = separar(datos_archivo, indices_a_seleccionar)
-    escribir_archivo('datafutbol.csv', datos_seleccionados)
+# Función principal
+if __name__ == "__main__":
+    datos_archivo = leer_archivo('./datafutbol.csv')
+    if datos_archivo:
+        indices_a_seleccionar = [3, 4, 5, 6, 7]
+        datos_seleccionados = separar(datos_archivo, indices_a_seleccionar)
+        escribir_archivo('datafutbol_process.csv', datos_seleccionados)
